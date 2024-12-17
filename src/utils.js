c1ca91d915d922a9453097fdb7079584da0a99a3
KB Master <proofofrandomness@gmail.com>
2024-12-17 08:39:07 +0200

    WIP

--- src/utils.js
+++ src/utils.js
@@ -398,7 +398,7 @@ async function modifyKB(kbToken, kbData, prompt, files, options) {
 
             if (newAssistantMessage?.content) {
                 lastProcessedAssistantMsgId = newAssistantMessage?.msgId;
-                const batchRegex = /(?:createModifyFile\s+(?<fileName>[^\s]+)\s*###(?<language>\w+)\s*(?<content>[\s\S]*?)###|\/?(?<actionType>metaResponse|operationCompleted|operationFailed|[a-zA-Z]{3,30})\((?<actionParams>[^()]*)\))/g;
+                const batchRegex = /(?:createModifyFile\s+(?<fileName>[^\s]+)\s*###(?<language>\w+)\s*(?<content>[\s\S]*?)###|\/?(?<type>metaResponse|operationCompleted|operationFailed|[a-zA-Z]{3,30})\((?<params>[^()]*)\))/g;
                 const blocks = Array.from(newAssistantMessage.content.matchAll(batchRegex), match => match.groups);
 
                 const showResponse = () => {
@@ -413,7 +413,7 @@ async function modifyKB(kbToken, kbData, prompt, files, options) {
                 }
 
                 for (const block of blocks) {
-                    if (['operationCompleted', 'operationFailed'].includes(block?.actionType)) {
+                    if (['operationCompleted', 'operationFailed'].includes(block?.type)) {
                         if (!preserveChat) await makePostRequest(url, {
                             action: 'deleteChat',
                             token: kbToken,
@@ -422,12 +422,12 @@ async function modifyKB(kbToken, kbData, prompt, files, options) {
 
                         stopLoading();
                         if (verbose) console.log('\nMessages:\n', decryptedMessages);
-                        console.log({ actionType: block.actionType, actionParams: JSON.parse(block.actionParams) });
+                        console.log({ type: block.type, params: JSON.parse(block.params) });
                         process.exit(0);
-                    } else if (block?.actionType === 'metaResponse' && block?.actionParams === 'execute_and_wait') {
+                    } else if (block?.type === 'metaResponse' && block?.params === 'execute_and_wait') {
                         showResponse();
                         await sendMessage(await getUserInput('\nYou: '));
-                    } else if (block?.fileName && block?.content && blocks?.find(o => o?.actionType === 'operationCompleted')) {
+                    } else if (block?.fileName && block?.content && blocks?.find(o => o?.type === 'operationCompleted')) {
                         await fs.outputFile(block.fileName, block.content);
                         console.log(`File written: ${block.fileName}`);
                     }
