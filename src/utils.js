6a344f4777bf72fffe3a22fe0031d2c1999bfb9b
KB Master <proofofrandomness@gmail.com>
2025-07-15 12:51:25 +0300

    WIP

--- src/utils.js
+++ src/utils.js
@@ -1085,6 +1085,15 @@ async function downloadTemplatesFromS3(targetDir) {
     
     // Wait for all downloads to complete
     await Promise.all(downloadPromises);
+    
+    // Add .openkbs/* to .gitignore
+    const gitignorePath = path.join(targetDir, '.gitignore');
+    if (await fs.pathExists(gitignorePath)) {
+        let content = await fs.readFile(gitignorePath, 'utf8');
+        if (!content.includes('.openkbs/*')) {
+            await fs.writeFile(gitignorePath, content.trim() + '\n.openkbs/*\n');
+        }
+    }
 }
 
 const generateKey = (passphrase) => {
